6f09efc51bd374b64f05be7b7b678a5bae4fe320
Robert Schönthal <robert.schoenthal@gmail.com>
2017-11-07 23:47:11 +0100

    fixed rollup script

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import {rollup} from 'rollup';
 
 import CONFIG from './rollup.config.js';
 
-export default rollup(CONFIG);
+const build = async () => {
+    const bundle = await rollup({input : CONFIG.input});
+    const { code, map } = await bundle.generate(CONFIG.output);
+
+    return await bundle.write(CONFIG.output);
+}
+
+export default build

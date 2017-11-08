cf5ba0f1ab097eb67f610ad9c3885d67fa487a56
Robert Schönthal <robert.schoenthal@gmail.com>
2017-11-08 14:39:42 +0100

    awesome comment

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import { rollup } from "rollup";
 
 import CONFIG from "./rollup.config.js";
 
+/**
+ * awesome build
+ */
 const build = async () => {
     const bundle = await rollup({input : CONFIG.input});
     const { code, map } = await bundle.generate(CONFIG.output);
